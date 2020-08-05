aadca4ee7b9432eeb3088e10e17b804b155f0c61
andicodetrf <andrea_lau92@hotmail.com>
2020-08-05 16:10:24 +0800

    updated Posts controller index, new, edit, destroy, Post Index view, Post New and Edit Forms

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,29 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :destroy, :update]
+
   def index
     @posts = Post.all
   end
 
   def new
+    @post = Post.new
+
   end
 
+  def create
+    @post = Post.new(post_params)
+    #post_params is saying this ONE post
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+
   def edit
+
   end
 
   def show
@@ -16,8 +33,22 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    if @post.destroys
+      redirect_to posts_path
+    else
+      render :index
+    end
   end
 
-  def create
+
+
+  private
+  def post_params
+    params.require(:post).permit(:post_image, :caption)
   end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,9 @@ Rails.application.routes.draw do
   resources :posts  do
     resources :comments
   end
+
+
+  resources :posts
+
+  root 'posts#index'
 end
