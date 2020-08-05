ef85dee5a61ed6b22415e6b2c2eaae19bf721870
siu-sing <lai.siusing@gmail.com>
2020-08-05 11:43:28 +0800

    added user, post and comment models

---
+++ db/migrate/20200805034006_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :email
+      t.string :phone
+      t.string :address
+      t.string :city
+      t.string :country
+
+      t.timestamps
+    end
+  end
+end
