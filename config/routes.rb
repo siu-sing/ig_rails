e18f4a307f637395478a4bb357da848df14a3de7
randallalala <randall.lim.lim.lim@hotmail.com>
2020-08-05 15:43:11 +0800

    signup done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/show'
+  get 'users/update'
+  get 'users/destroy'
+  get 'users/create'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
