73ea72c639ebca636c83efd9c171e2a9efb7e146
JiaHern <hojiahern@gmail.com>
2020-08-05 15:41:16 +0800

    added_comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# ActiveRecord::Schema.define(version: 2020_08_05_034152) do
 ActiveRecord::Schema.define(version: 2020_08_05_052433) do
 
   # These are extensions that must be enabled in order to support this database
@@ -42,5 +41,6 @@ ActiveRecord::Schema.define(version: 2020_08_05_052433) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
   add_foreign_key "comments", "posts"
 end
