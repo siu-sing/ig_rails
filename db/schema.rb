65265435e49659f1272bdc0a2cb337923f84b5fb
andicodetrf <andrea_lau92@hotmail.com>
2020-08-06 17:05:25 +0800

    validation for current user for edit/delete own post

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_08_06_055739) do
-
+ActiveRecord::Schema.define(version: 2020_08_06_062205) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,5 +79,4 @@ ActiveRecord::Schema.define(version: 2020_08_06_055739) do
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
-
 end
